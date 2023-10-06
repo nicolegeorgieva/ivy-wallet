097d6e90600a5349831b80ffb1fb4ca9a6c75021
Nicole <nicole.georgievva@gmail.com>
2023-10-06 16:01:39 +0300

    WIP: TransactionMapper

--- ivy-data/src/main/java/com/ivy/data/repository/mapper/TransactionMapper.kt
+++ ivy-data/src/main/java/com/ivy/data/repository/mapper/TransactionMapper.kt
@@ -1,16 +1,51 @@
 package com.ivy.data.repository.mapper
 
 import arrow.core.Either
+import arrow.core.raise.either
+import com.ivy.base.model.TransactionType
 import com.ivy.data.db.entity.TransactionEntity
+import com.ivy.data.model.CategoryId
 import com.ivy.data.model.Transaction
+import com.ivy.data.model.TransactionId
+import com.ivy.data.model.TransactionMetadata
+import com.ivy.data.model.primitive.NotBlankTrimmedString
+import java.time.Instant
 import javax.inject.Inject
 
 class TransactionMapper @Inject constructor() {
-    fun TransactionEntity.toDomain(): Either<String, Transaction> {
-
+    fun TransactionEntity.toDomain(): Either<String, Transaction> = either {
+        Transaction(
+            id = TransactionId(id),
+            title = title?.let { NotBlankTrimmedString.from(it) },
+            description = description?.let { NotBlankTrimmedString.from(it) },
+            category = CategoryId(categoryId),
+            time = Instant.EPOCH,
+            settled =,
+            metadata = TransactionMetadata,
+            lastUpdated = Instant.EPOCH,
+            removed = isDeleted
+        )
     }
 
     fun Transaction.toEntity(): TransactionEntity {
-
+        return TransactionEntity(
+            accountId =,
+            type = TransactionType,
+            amount = Double,
+            toAccountId = null,
+            toAmount = null,
+            title = null,
+            description = description?.value,
+            dateTime = time,
+            categoryId = category?.value,
+            dueDate = null,
+            recurringRuleId = null,
+            attachmentUrl = null,
+            loanId = null,
+            loanRecordId = null,
+            isSynced = false,
+            isDeleted = removed,
+            id = id.value
+        )
     }
 }
