dc7c6d827141fb900df247572a65cf87a838e612
Nicole <nicole.georgievva@gmail.com>
2023-10-06 19:40:13 +0300

    WIP: TransactionMapper

--- ivy-data/src/main/java/com/ivy/data/repository/mapper/TransactionMapper.kt
+++ ivy-data/src/main/java/com/ivy/data/repository/mapper/TransactionMapper.kt
@@ -4,36 +4,66 @@ import arrow.core.Either
 import arrow.core.raise.either
 import com.ivy.base.model.TransactionType
 import com.ivy.data.db.entity.TransactionEntity
+import com.ivy.data.model.AccountId
 import com.ivy.data.model.CategoryId
+import com.ivy.data.model.Expense
+import com.ivy.data.model.Income
 import com.ivy.data.model.Transaction
 import com.ivy.data.model.TransactionId
 import com.ivy.data.model.TransactionMetadata
+import com.ivy.data.model.Transfer
+import com.ivy.data.model.common.Value
+import com.ivy.data.model.primitive.AssetCode
 import com.ivy.data.model.primitive.NotBlankTrimmedString
+import com.ivy.data.model.primitive.PositiveDouble
 import java.time.Instant
 import javax.inject.Inject
 
 class TransactionMapper @Inject constructor() {
     fun TransactionEntity.toDomain(): Either<String, Transaction> = either {
         when (type) {
-            TransactionType.INCOME ->
-                TransactionType.EXPENSE
+            TransactionType.INCOME -> Income(
+                id = TransactionId(id),
+                title = title?.let { NotBlankTrimmedString(it) },
+                description = description?.let { NotBlankTrimmedString(it) },
+                category = categoryId?.let { CategoryId(it) },
+                time = Instant.EPOCH,
+                settled = false,// TODO
+                metadata = TransactionMetadata(recurringRuleId, loanId, loanRecordId),
+                lastUpdated = Instant.EPOCH,// TODO
+                removed = isDeleted,
+                value = Value(amount = PositiveDouble(amount), asset = AssetCode(""))// TODO
+            )
 
-            ->
-                TransactionType.TRANSFER
+            TransactionType.EXPENSE -> Expense(
+                id = TransactionId(id),
+                title = title?.let { NotBlankTrimmedString(it) },
+                description = description?.let { NotBlankTrimmedString(it) },
+                category = categoryId?.let { CategoryId(it) },
+                time = Instant.EPOCH,
+                settled = false,// TODO
+                metadata = TransactionMetadata(recurringRuleId, loanId, loanRecordId),
+                lastUpdated = Instant.EPOCH,// TODO
+                removed = isDeleted,
+                value = Value(amount = PositiveDouble(amount), asset = AssetCode(""))// TODO
+            )
 
-            ->
+            TransactionType.TRANSFER -> Transfer(
+                id = TransactionId(id),
+                title = title?.let { NotBlankTrimmedString(it) },
+                description = description?.let { NotBlankTrimmedString(it) },
+                category = categoryId?.let { CategoryId(it) },
+                time = Instant.EPOCH,
+                settled = false,// TODO
+                metadata = TransactionMetadata(recurringRuleId, loanId, loanRecordId),
+                lastUpdated = Instant.EPOCH,// TODO
+                removed = isDeleted,
+                fromAccount = AccountId(accountId),
+                fromValue = Value(amount = PositiveDouble(amount), asset = AssetCode("")),
+                toAccount = AccountId(accountId),
+                toValue = Value(amount = PositiveDouble(amount), asset = AssetCode(""))
+            )
         }
-        Transaction(
-            id = TransactionId(id),
-            title = title?.let { NotBlankTrimmedString.from(it) },
-            description = description?.let { NotBlankTrimmedString.from(it) },
-            category = CategoryId(categoryId),
-            time = Instant.EPOCH,
-            settled =,
-            metadata = TransactionMetadata,
-            lastUpdated = Instant.EPOCH,
-            removed = isDeleted
-        )
     }
 
     fun Transaction.toEntity(): TransactionEntity {
