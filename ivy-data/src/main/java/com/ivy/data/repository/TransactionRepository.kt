f9432e3ba3a4ed20f7aabece9bddb5a941348d15
Nicole <nicole.georgievva@gmail.com>
2023-10-06 15:14:32 +0300

    WIP: TransactionRepository

--- ivy-data/src/main/java/com/ivy/data/repository/TransactionRepository.kt
+++ ivy-data/src/main/java/com/ivy/data/repository/TransactionRepository.kt
@@ -1,7 +1,10 @@
 package com.ivy.data.repository
 
 import com.ivy.base.model.TransactionType
+import com.ivy.data.model.AccountId
+import com.ivy.data.model.CategoryId
 import com.ivy.data.model.Transaction
+import com.ivy.data.model.TransactionId
 import java.time.LocalDateTime
 import java.util.UUID
 
@@ -10,18 +13,18 @@ interface TransactionRepository {
     suspend fun findAllLimit1(): List<Transaction>
     suspend fun findAllByTypeAndAccount(
         type: TransactionType,
-        accountId: UUID
+        accountId: AccountId
     ): List<Transaction>
 
     suspend fun findAllByTypeAndAccountBetween(
         type: TransactionType,
-        accountId: UUID,
+        accountId: AccountId,
         startDate: LocalDateTime,
         endDate: LocalDateTime
     ): List<Transaction>
 
     suspend fun findAllTransfersToAccount(
-        toAccountId: UUID,
+        toAccountId: AccountId,
         type: TransactionType = TransactionType.TRANSFER
     ): List<Transaction>
 
@@ -31,13 +34,13 @@ interface TransactionRepository {
     ): List<Transaction>
 
     suspend fun findAllByAccountAndBetween(
-        accountId: UUID,
+        accountId: AccountId,
         startDate: LocalDateTime,
         endDate: LocalDateTime
     ): List<Transaction>
 
     suspend fun findAllByCategoryAndBetween(
-        categoryId: UUID,
+        categoryId: CategoryId,
         startDate: LocalDateTime,
         endDate: LocalDateTime
     ): List<Transaction>
@@ -48,7 +51,7 @@ interface TransactionRepository {
     ): List<Transaction>
 
     suspend fun findAllByCategoryAndTypeAndBetween(
-        categoryId: UUID,
+        categoryId: CategoryId,
         type: TransactionType,
         startDate: LocalDateTime,
         endDate: LocalDateTime
@@ -61,7 +64,7 @@ interface TransactionRepository {
     ): List<Transaction>
 
     suspend fun findAllToAccountAndBetween(
-        toAccountId: UUID,
+        toAccountId: AccountId,
         startDate: LocalDateTime,
         endDate: LocalDateTime
     ): List<Transaction>
@@ -74,7 +77,7 @@ interface TransactionRepository {
     suspend fun findAllDueToBetweenByCategory(
         startDate: LocalDateTime,
         endDate: LocalDateTime,
-        categoryId: UUID
+        categoryId: CategoryId
     ): List<Transaction>
 
     suspend fun findAllDueToBetweenByCategoryUnspecified(
@@ -85,32 +88,32 @@ interface TransactionRepository {
     suspend fun findAllDueToBetweenByAccount(
         startDate: LocalDateTime,
         endDate: LocalDateTime,
-        accountId: UUID
+        accountId: AccountId
     ): List<Transaction>
 
     suspend fun findAllByRecurringRuleId(recurringRuleId: UUID): List<Transaction>
-    suspend fun findById(id: UUID): Transaction?
+    suspend fun findById(id: TransactionId): Transaction?
     suspend fun countHappenedTransactions(): Long
     suspend fun findAllByTitleMatchingPattern(pattern: String): List<Transaction>
     suspend fun countByTitleMatchingPattern(pattern: String): Long
-    suspend fun findAllByCategory(categoryId: UUID): List<Transaction>
+    suspend fun findAllByCategory(categoryId: CategoryId): List<Transaction>
     suspend fun countByTitleMatchingPatternAndCategoryId(
         pattern: String,
-        categoryId: UUID
+        categoryId: CategoryId
     ): Long
 
-    suspend fun findAllByAccount(accountId: UUID): List<Transaction>
-    suspend fun countByTitleMatchingPatternAndAccountId(pattern: String, accountId: UUID): Long
+    suspend fun findAllByAccount(accountId: AccountId): List<Transaction>
+    suspend fun countByTitleMatchingPatternAndAccountId(pattern: String, accountId: AccountId): Long
     suspend fun findLoanTransaction(loanId: UUID): Transaction?
     suspend fun findLoanRecordTransaction(loanRecordId: UUID): Transaction?
     suspend fun findAllByLoanId(loanId: UUID): List<Transaction>
 
     suspend fun save(value: Transaction)
     suspend fun saveMany(value: List<Transaction>)
-    suspend fun flagDeleted(id: UUID)
+    suspend fun flagDeleted(id: TransactionId)
     suspend fun flagDeletedByRecurringRuleIdAndNoDateTime(recurringRuleId: UUID)
-    suspend fun flagDeletedByAccountId(accountId: UUID)
-    suspend fun deleteById(id: UUID)
-    suspend fun deleteAllByAccountId(accountId: UUID)
+    suspend fun flagDeletedByAccountId(accountId: AccountId)
+    suspend fun deleteById(id: TransactionId)
+    suspend fun deleteAllByAccountId(accountId: AccountId)
     suspend fun deleteAll()
 }
